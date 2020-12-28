d91a29f0092be88dc0184ca81e014edc5ee05bba
Harvey Rendell <harvey.rendell@pushpay.com>
2020-12-28 19:28:41 +1300

    Refactor webhook into function, prepare for webhook persistence

--- events/handler.py
+++ events/handler.py
@@ -18,6 +18,22 @@ STRAVA_API_BASE = "https://www.strava.com/api/v3"
 
 logger = logging.getLogger(__name__)
 
+# See https://developers.strava.com/docs/reference/#api-models-ActivityType
+activity_colours = {
+    "Run": 0xFC4C02,  # orange
+    "Ride": 0x66C2FF,  # pale blue
+    "Hike": 0x008000,  # forest green
+    "RockClimbing": 0xFF8000,  # rock colour?
+    "AlpineSki": 0xFEFEFE,  # snow
+    "BackcountrySki": 0xFEFEFE,  # snow
+    "NordicSki": 0xFEFEFE,  # snow
+    "Snowboard": 0xFEFEFE,  # snow
+    "default": 0xFC4C02,  # also orange
+}
+
+# Activity types to use average speed instead of pace
+use_speed = ["Ride"]
+
 
 def subscribe(event, *_):
     """Respond to a Strava subscription validation request.
@@ -92,8 +108,8 @@ def get_token_for_athlete(id):
     logger.info("Getting token")
 
     dynamodb = boto3.resource("dynamodb")
-    table = dynamodb.Table(os.environ["DYNAMODB_TABLE"])
-    result = table.get_item(Key={"id": id})
+    users_table = dynamodb.Table(os.environ["USERS_DYNAMODB_TABLE"])
+    result = users_table.get_item(Key={"id": id})
     token_expiry = datetime.fromtimestamp(result["Item"]["expires_at"])
     expired = token_expiry < datetime.now()
 
@@ -124,7 +140,7 @@ def get_token_for_athlete(id):
         item["access_token"] = response_body["access_token"]
         item["refresh_token"] = response_body["refresh_token"]
         item["expires_at"] = response_body["expires_at"]
-        table.put_item(Item=item)
+        users_table.put_item(Item=item)
 
         access_token = response_body["access_token"]
     else:
@@ -136,104 +152,89 @@ def get_token_for_athlete(id):
 
 
 def post_message(body):
-    # See https://developers.strava.com/docs/reference/#api-models-ActivityType
-    activity_colours = {
-        "Run": 0xFC4C02,  # orange
-        "Ride": 0x66C2FF,  # pale blue
-        "Hike": 0x008000,  # forest green
-        "RockClimbing": 0xFF8000,  # rock colour?
-        "AlpineSki": 0xFEFEFE,  # snow
-        "BackcountrySki": 0xFEFEFE,  # snow
-        "NordicSki": 0xFEFEFE,  # snow
-        "Snowboard": 0xFEFEFE,  # snow
-        "default": 0xFC4C02,  # also orange
-    }
-
-    # Activity types to use average speed instead of pace
-    use_speed = ["Ride"]
-
     object_type = body["object_type"]  # one of "activity" or "athlete"
     object_id = body["object_id"]  # id for specified object_type
-
     access_token = get_token_for_athlete(body["owner_id"])
 
     if object_type == "activity":
-        activity_resp = requests.get(
-            f"{STRAVA_API_BASE}/activities/{object_id}",
-            headers={"Authorization": f"Bearer {access_token}"},
-        )
-        athlete_resp = requests.get(
-            f"{STRAVA_API_BASE}/athlete",
-            headers={"Authorization": f"Bearer {access_token}"},
-        )
-
-        activity = activity_resp.json()
-        athlete = athlete_resp.json()
-
-        # Calculate displayed distance
-        activity_distance_km = round(activity["distance"] / 1000, 2)
-
-        # Calculate displayed moving time
-        hours, rem = divmod(activity["moving_time"], 3600)
-        minutes, seconds = divmod(rem, 60)
-        time_array = (
-            [hours, "{:02d}".format(minutes), "{:02d}".format(seconds)]
-            if hours
-            else [minutes, "{:02d}".format(seconds)]
-        )  # add leading zeroes in time format
-        activity_moving_time = ":".join(str(v) for v in time_array)
-
-        # calculate displayed pace
-        activity_minutes = activity["moving_time"] / 60
-        raw_pace = activity_minutes / activity_distance_km
-        pace_minutes, pace_seconds = divmod(raw_pace, 1)
-        pace_seconds = round(pace_seconds * 0.6, 2)  # convert to seconds from decimal
-        activity_pace = f"{int(pace_minutes)}:{int(pace_seconds * 100):02d}"
-        activity_speed_kmh = round(
-            activity["average_speed"] * 3.6, 1
-        )  # convert from m/s
-
-        elevation = activity["total_elevation_gain"]
-        activity_type = activity["type"]
-
-        # Build new embed
-
-        embed = discord.Embed(
-            title=activity["name"],
-            url=f"https://strava.com/activities/{activity['id']}",
-            colour=activity_colours.get(activity_type, activity_colours["default"]),
-        )
-        embed.timestamp = datetime.strptime(
-            activity["start_date"], "%Y-%m-%dT%H:%M:%SZ"
-        )
-        embed.set_author(
-            name=f"{athlete['firstname']} {athlete['lastname']}",
-            url=f"https://strava.com/athletes/{athlete['id']}",
-            icon_url=athlete["profile_medium"],
-        )
-        embed.set_footer(
-            text="Powered by Strava",
-            icon_url="https://d3nn82uaxijpm6.cloudfront.net/apple-touch-icon-144x144.png?v=dLlWydWlG8",
-        )
-        embed.add_field(name="Distance", value=f"{activity_distance_km} km", inline=True)
-        embed.add_field(name="Moving Time", value=activity_moving_time, inline=True)
-
-        if activity_type in use_speed:
-            embed.add_field(name="Average Speed", value=f"{activity_speed_kmh} km/h", inline=True)
-        else:
-            embed.add_field(name="Pace", value=f"{activity_pace} /km", inline=True)
-
-        embed.add_field(name="Elevation", value=f"{elevation} m", inline=True)
-
+        embed = build_webhook_message(access_token, object_id)
     elif object_type == "athlete":
         pass
 
-    new_post_webhook(embed)
+    post_webhook(embed)
 
     return 200
 
 
-def new_post_webhook(embed):
+def build_webhook_message(access_token, object_id):
+    activity = requests.get(
+        f"{STRAVA_API_BASE}/activities/{object_id}",
+        headers={"Authorization": f"Bearer {access_token}"},
+    ).json()
+
+    athlete = requests.get(
+        f"{STRAVA_API_BASE}/athlete",
+        headers={"Authorization": f"Bearer {access_token}"},
+    ).json()
+
+    # Calculate displayed distance
+    activity_distance_km = round(activity["distance"] / 1000, 2)
+
+    # Calculate displayed moving time
+    hours, rem = divmod(activity["moving_time"], 3600)
+    minutes, seconds = divmod(rem, 60)
+    time_array = (
+        [hours, "{:02d}".format(minutes), "{:02d}".format(seconds)]
+        if hours
+        else [minutes, "{:02d}".format(seconds)]
+    )  # add leading zeroes in time format
+    activity_moving_time = ":".join(str(v) for v in time_array)
+
+    # calculate displayed pace
+    activity_minutes = activity["moving_time"] / 60
+    raw_pace = activity_minutes / activity_distance_km
+    pace_minutes, pace_seconds = divmod(raw_pace, 1)
+    pace_seconds = round(pace_seconds * 0.6, 2)  # convert to seconds from decimal
+    activity_pace = f"{int(pace_minutes)}:{int(pace_seconds * 100):02d}"
+    activity_speed_kmh = round(activity["average_speed"] * 3.6, 1) # convert from m/s
+
+    elevation = activity["total_elevation_gain"]
+
+    activity_type = activity["type"]
+
+    # Build new embed
+    embed = discord.Embed(
+        title=activity["name"],
+        url=f"https://strava.com/activities/{activity['id']}",
+        colour=activity_colours.get(activity_type, activity_colours["default"]),
+    )
+    embed.timestamp = datetime.strptime(
+        activity["start_date"], "%Y-%m-%dT%H:%M:%SZ"
+    )
+    embed.set_author(
+        name=f"{athlete['firstname']} {athlete['lastname']}",
+        url=f"https://strava.com/athletes/{athlete['id']}",
+        icon_url=athlete["profile_medium"],
+    )
+    embed.set_footer(
+        text="Powered by Strava",
+        icon_url="https://d3nn82uaxijpm6.cloudfront.net/apple-touch-icon-144x144.png?v=dLlWydWlG8",
+    )
+
+    embed.add_field(name="Distance", value=f"{activity_distance_km} km", inline=True)
+    embed.add_field(name="Moving Time", value=activity_moving_time, inline=True)
+
+    if activity_type in use_speed:
+        embed.add_field(name="Average Speed", value=f"{activity_speed_kmh} km/h", inline=True)
+    else:
+        embed.add_field(name="Pace", value=f"{activity_pace} /km", inline=True)
+
+    embed.add_field(name="Elevation", value=f"{elevation} m", inline=True)
+
+    return embed
+
+
+def post_webhook(embed):
     webhook = discord.Webhook.from_url(
         DISCORD_WEBHOOK_URL, adapter=discord.RequestsWebhookAdapter()
     )
