0b57f7c7cad24cad763613d73dec6cb288a42286
Luke Wolyncewicz <luke@bumblepie.space>
2024-07-17 06:47:12 +1200

    Add a test to ensure webhook embed builder doesnt crash with minimal activity data

---
+++ tests/test_minimal_data_doesnt_crash.py
@@ -0,0 +1,18 @@
+from events.handler import build_webhook_message
+
+
+def test_minimal_data_doesnt_crash_building_embed():
+    activity = {
+        "name": "Some Activity",
+        "description": "Some exercise I guess.",
+        "start_date": "2024-01-01T00:00:00Z",
+        "type": "Some weird type",
+        "id": 0,
+    }
+    athlete = {
+        "firstname": "test",
+        "lastname": "person",
+        "id": 1,
+        "profile_medium": "https://strava.com/link-to-profile-pic"
+    }
+    build_webhook_message(activity, athlete)
