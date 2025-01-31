865277caecc69d117e4251eeac9dce704c7691d4
Harvey Rendell <harvey.rendell@pushpay.com>
2025-01-31 21:47:20 +1300

    Add more map tests

--- tests/test_map.py
+++ tests/test_map.py
@@ -11,3 +11,18 @@ def test_get_activity_map_url(monkeypatch):
     result = get_activity_map_url(activity)
     expected_url = "https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/path-3+FC4800-1(encoded_polyline)/auto/544x218?access_token=test_token"
     assert result == expected_url
+
+def test_get_activity_map_url_no_map():
+    activity = {}
+    result = get_activity_map_url(activity)
+    assert result is None
+
+def test_get_activity_map_url_long_url(monkeypatch):
+    activity = {
+        "map": {
+            "summary_polyline": "a" * 2049
+        }
+    }
+    monkeypatch.setattr("events.handler.MAPBOX_ACCESS_TOKEN", "test_token")
+    result = get_activity_map_url(activity)
+    assert result is None
